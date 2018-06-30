d4c82767f0569f5f1204b01320f22fc9e33f3b89
Niek Candaele <niekcandaele@gmail.com>
2018-06-30 13:35:43 +0200

    Fix getPlayersLocation test

--- test/getPlayersLocation.test.ts
+++ test/getPlayersLocation.test.ts
@@ -1,21 +1,21 @@
 'use strict';
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
-import { SdtdServer } from '../lib/index'
-let SdtdApi = require('../lib/index.js');
+import * as SdtdApi from '../lib/index'
+//let SdtdApi = require('../lib/index.js');
 
 require('dotenv').config()
 
 chai.use(chaiAsPromised)
 
-let testServer: SdtdServer = {
+let testServer: SdtdApi.SdtdServer = {
     ip: process.env.TESTIP as String,
     port: process.env.TESTPORT as String,
     adminUser: process.env.TESTADMINUSER as String,
     adminToken: process.env.TESTADMINTOKEN as String
 }
 
-let badTestServer: SdtdServer = {
+let badTestServer: SdtdApi.SdtdServer = {
     ip: "Not an IP address",
     port: process.env.TESTPORT as String,
     adminUser: process.env.TESTADMINUSER as String,
@@ -24,7 +24,7 @@ let badTestServer: SdtdServer = {
 
 describe('/api/getPlayersLocation', async () => {
     it('Returns an array', async () => {
-        let response = await SdtdApi.getPlayersLocation(testServer);
+        let response = await SdtdApi.getPlayersLocation(testServer, false);
         chai.expect(response).to.be.a('array');
     });
     it('Returns an array with data if offline is set to true', async () => {
@@ -36,6 +36,6 @@ describe('/api/getPlayersLocation', async () => {
         chai.expect(response[0].online).to.be.a("boolean");
     });
     it('Errors when incorrect server info is given', async () => {
-        return chai.expect(SdtdApi.getPlayersLocation(badTestServer)).to.be.rejectedWith(Error);
+        return chai.expect(SdtdApi.getPlayersLocation(badTestServer, false)).to.be.rejectedWith(Error);
     });
 });
