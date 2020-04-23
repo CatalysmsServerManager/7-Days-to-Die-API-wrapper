8b5af7019931663039b4fc1fff861978de9d7ceb
Gavin Mogan <git@gavinmogan.com>
2020-04-22 17:38:54 -0700

    Add support for forceHttps

--- lib/index.ts
+++ lib/index.ts
@@ -4,81 +4,94 @@ import * as responses from './responses'
 export interface SdtdServer {
     ip: String,
     port: String,
+    forceHttps?: Boolean,
     adminUser: String,
     adminToken: String
 }
 
-async function fetchJson(url : string, fetchOpts?: RequestInit) {
-    const uri = encodeURI(url);
+export function getBaseUrl(server: SdtdServer): String {
+    let scheme = "http";
+    if (server.forceHttps === true) {
+        scheme = "https"
+    } else if (server.forceHttps === false) {
+        scheme = "http"
+    } else if (server.port === "443") {
+        scheme = "https"
+    }
+    return `${scheme}://${server.ip}:${server.port}`;
+}
+
+async function fetchJson(server: SdtdServer, url: string, fetchOpts?: RequestInit) {
+    const uri = getBaseUrl(server) + encodeURI(url);
     return await fetch(uri, fetchOpts).then(res => res.json())
 }
 
 export async function getStats(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getstats?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getstats?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as responses.StatsResponse
 }
 
 export async function getOnlinePlayers(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getplayersonline?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getplayersonline?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as Array<responses.OnlinePlayerResponse>
 }
 
 export async function getAllowedCommands(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getallowedcommands?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getallowedcommands?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as responses.AllowedCommands
 }
 
 export async function executeConsoleCommand(server: SdtdServer, command: String, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/executeconsolecommand?adminuser=${server.adminUser}&admintoken=${server.adminToken}&command=${command}`, fetchOpts)
+    const response = await fetchJson(server, `/api/executeconsolecommand?adminuser=${server.adminUser}&admintoken=${server.adminToken}&command=${command}`, fetchOpts)
     return response as responses.CommandResponse
 }
 
 export async function getAnimalsLocation(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getanimalslocation?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getanimalslocation?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as Array<responses.entityLocation>
 }
 
 export async function getHostileLocation(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/gethostilelocation?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/gethostilelocation?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as Array<responses.entityLocation>
 }
 
 export async function getLandClaims(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getlandclaims?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getlandclaims?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as Array<responses.landClaimsResponse>
 }
 
 export async function getPlayerInventory(server: SdtdServer, steamId: String, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getplayerinventory?adminuser=${server.adminUser}&admintoken=${server.adminToken}&steamId=${steamId}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getplayerinventory?adminuser=${server.adminUser}&admintoken=${server.adminToken}&steamId=${steamId}`, fetchOpts)
     return response as Array<responses.InventoryResponse>
 }
 
 export async function getPlayerInventories(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getplayerinventories?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getplayerinventories?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as responses.GetLog
 }
 
 export async function getPlayerList(server: SdtdServer, rowsPerPage: Number, page: Number, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getplayerlist?adminuser=${server.adminUser}&admintoken=${server.adminToken}&rowsperpage=${rowsPerPage}&page=${page}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getplayerlist?adminuser=${server.adminUser}&admintoken=${server.adminToken}&rowsperpage=${rowsPerPage}&page=${page}`, fetchOpts)
     return response as Array<responses.getPlayerListResponse>
 }
 
 export async function getPlayersLocation(server: SdtdServer, offline: Boolean, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getplayerslocation?adminuser=${server.adminUser}&admintoken=${server.adminToken}&offline=${offline}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getplayerslocation?adminuser=${server.adminUser}&admintoken=${server.adminToken}&offline=${offline}`, fetchOpts)
     return response as Array<responses.PlayerLocation>
 }
 
 export async function getServerInfo(server: SdtdServer, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getserverinfo?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getserverinfo?adminuser=${server.adminUser}&admintoken=${server.adminToken}`, fetchOpts)
     return response as responses.GetServerInfo
 }
 
 export async function getWebUIUpdates(server: SdtdServer, latestLine: Number, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getwebuiupdates?adminuser=${server.adminUser}&admintoken=${server.adminToken}&latestLine=${latestLine}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getwebuiupdates?adminuser=${server.adminUser}&admintoken=${server.adminToken}&latestLine=${latestLine}`, fetchOpts)
     return response as responses.GetWebUIUpdatesResponse
 }
 
 export async function getLog(server: SdtdServer, firstLine: Number, count: Number = 50, fetchOpts?: RequestInit) {
-    const response = await fetchJson(`http://${server.ip}:${server.port}/api/getlog?adminuser=${server.adminUser}&admintoken=${server.adminToken}&firstLine=${firstLine}&count=${count}`, fetchOpts)
+    const response = await fetchJson(server, `/api/getlog?adminuser=${server.adminUser}&admintoken=${server.adminToken}&firstLine=${firstLine}&count=${count}`, fetchOpts)
     return response as Array<responses.InventoryResponse>
 }
---
+++ test/getBaseUrl.test.ts
@@ -0,0 +1,36 @@
+'use strict';
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+import { getBaseUrl, SdtdServer } from '../lib/index'
+
+chai.use(chaiAsPromised)
+
+let testServer: SdtdServer = {
+    ip: process.env.TESTIP as String,
+    port: process.env.TESTPORT as String,
+    adminUser: process.env.TESTADMINUSER as String,
+    adminToken: process.env.TESTADMINTOKEN as String
+}
+
+describe('getBaseUrl', async () => {
+    it('standard returns http', async () => {
+        chai.expect(
+            getBaseUrl({ ip: '192.168.1.1', port: '5432', adminUser: 'foo', adminToken: 'bar' })
+        ).to.eql('http://192.168.1.1:5432');
+    });
+    it('standard but port 443 returns https', async () => {
+        chai.expect(
+            getBaseUrl({ ip: '192.168.1.1', port: '443', adminUser: 'foo', adminToken: 'bar' })
+        ).to.eql('https://192.168.1.1:443');
+    });
+    it('forced true returns https', async () => {
+        chai.expect(
+            getBaseUrl({ ip: '192.168.1.1', port: '5432', adminUser: 'foo', adminToken: 'bar', forceHttps: true })
+        ).to.eql('https://192.168.1.1:5432');
+    });
+    it('forced false returns http', async () => {
+        chai.expect(
+            getBaseUrl({ ip: '192.168.1.1', port: '5432', adminUser: 'foo', adminToken: 'bar', forceHttps: false })
+        ).to.eql('http://192.168.1.1:5432');
+    });
+});
