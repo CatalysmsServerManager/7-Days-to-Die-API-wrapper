c0b9652f962bf48137d885d7120d9905692b150e
Gavin Mogan <git@gavinmogan.com>
2020-04-23 23:34:57 -0700

    consistant spacing around {}

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
-import {ParsedUrlQueryInput} from "querystring";
-import {stringify} from "qs";
+import { ParsedUrlQueryInput } from "querystring";
+import { stringify } from "qs";
 import fetch, { RequestInit } from "node-fetch"
 import * as responses from './responses'
 
@@ -32,15 +32,15 @@ async function fetchJson(server: SdtdServer, url: string, qs: ParsedUrlQueryInpu
 }
 
 export async function getStats(server: SdtdServer, fetchOpts?: RequestInit): Promise<responses.StatsResponse> {
-    return fetchJson(server, `/api/getstats`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/getstats`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function getOnlinePlayers(server: SdtdServer, fetchOpts?: RequestInit): Promise<Array<responses.OnlinePlayerResponse>> {
-    return fetchJson(server, `/api/getplayersonline`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/getplayersonline`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function getAllowedCommands(server: SdtdServer, fetchOpts?: RequestInit): Promise<responses.AllowedCommands> {
-    return fetchJson(server, `/api/getallowedcommands`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/getallowedcommands`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function executeConsoleCommand(server: SdtdServer, command: string, fetchOpts?: RequestInit): Promise<responses.CommandResponse> {
@@ -48,39 +48,39 @@ export async function executeConsoleCommand(server: SdtdServer, command: string,
 }
 
 export async function getAnimalsLocation(server: SdtdServer, fetchOpts?: RequestInit): Promise<Array<responses.EntityLocation>> {
-    return fetchJson(server, `/api/getanimalslocation`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/getanimalslocation`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function getHostileLocation(server: SdtdServer, fetchOpts?: RequestInit): Promise<Array<responses.EntityLocation>> {
-    return fetchJson(server, `/api/gethostilelocation`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/gethostilelocation`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function getLandClaims(server: SdtdServer, steamId?: string, fetchOpts?: RequestInit): Promise<responses.LandClaimsResponse> {
-    return fetchJson(server, `/api/getlandclaims`, { adminuser: server.adminUser,admintoken: server.adminToken, steamid: steamId}, fetchOpts)
+    return fetchJson(server, `/api/getlandclaims`, { adminuser: server.adminUser,admintoken: server.adminToken, steamid: steamId }, fetchOpts)
 }
 
 export async function getPlayerInventory(server: SdtdServer, steamId: string, fetchOpts?: RequestInit): Promise<responses.InventoryResponse> {
-    return fetchJson(server, `/api/getplayerinventory`, { adminuser: server.adminUser,admintoken: server.adminToken, steamid: steamId}, fetchOpts)
+    return fetchJson(server, `/api/getplayerinventory`, { adminuser: server.adminUser,admintoken: server.adminToken, steamid: steamId }, fetchOpts)
 }
 
 export async function getPlayerInventories(server: SdtdServer, fetchOpts?: RequestInit): Promise<responses.GetLog> {
-    return fetchJson(server, `/api/getplayerinventories`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/getplayerinventories`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function getPlayerList(server: SdtdServer, rowsPerPage?: number, page?: number, fetchOpts?: RequestInit): Promise<responses.PlayerListResponse> {
-    return fetchJson(server, `/api/getplayerlist`, { adminuser: server.adminUser,admintoken: server.adminToken, rowsperpage: rowsPerPage, page: page}, fetchOpts)
+    return fetchJson(server, `/api/getplayerlist`, { adminuser: server.adminUser,admintoken: server.adminToken, rowsperpage: rowsPerPage, page: page }, fetchOpts)
 }
 
 export async function getPlayersLocation(server: SdtdServer, offline: boolean, fetchOpts?: RequestInit): Promise<Array<responses.PlayerLocation>> {
-    return fetchJson(server, `/api/getplayerslocation`, { adminuser: server.adminUser,admintoken: server.adminToken, offline: offline}, fetchOpts)
+    return fetchJson(server, `/api/getplayerslocation`, { adminuser: server.adminUser,admintoken: server.adminToken, offline: offline }, fetchOpts)
 }
 
 export async function getServerInfo(server: SdtdServer, fetchOpts?: RequestInit): Promise<responses.GetServerInfo> {
-    return fetchJson(server, `/api/getserverinfo`, { adminuser: server.adminUser,admintoken: server.adminToken}, fetchOpts)
+    return fetchJson(server, `/api/getserverinfo`, { adminuser: server.adminUser,admintoken: server.adminToken }, fetchOpts)
 }
 
 export async function getWebUIUpdates(server: SdtdServer, latestLine?: number, fetchOpts?: RequestInit): Promise<responses.GetWebUIUpdatesResponse> {
-    return fetchJson(server, `/api/getwebuiupdates`, { adminuser: server.adminUser,admintoken: server.adminToken, latestLine: latestLine}, fetchOpts)
+    return fetchJson(server, `/api/getwebuiupdates`, { adminuser: server.adminUser,admintoken: server.adminToken, latestLine: latestLine }, fetchOpts)
 }
 
 export async function getLog(server: SdtdServer, firstLine?: number, count: number = 50, fetchOpts?: RequestInit): Promise<responses.GetLog> {
